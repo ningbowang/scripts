41e4cfa296167fd8c8f195822fef73f13ffe949c
ningbowang <47706456+ningbowang@users.noreply.github.com>
2022-07-19 09:08:50 +0800

    添加右键双击关闭标签页，但是chrome的原因这种方式只能禁用右键菜单，不好用

--- closeTheBrowserTab.user.js
+++ closeTheBrowserTab.user.js
@@ -29,3 +29,89 @@
   }
 })
 })()
+
+// // ==UserScript==
+// // @name        右键双击关闭标签页
+// // @author      Arden
+// // @namespace   Violentmonkey Scripts
+// // @match       *://*/*
+// // @run-at      document-start
+// // @grant       none
+// // @version     1.0.0
+// // @author      Arden
+// // @description 2022/7/10 12:34:19
+// // ==/UserScript==
+// // mode = 1 单机中键关闭当前标签页；
+// // mode = 2 双击右键关闭当前标签页
+// const mode = 2
+// function closeCurrentTab() {
+//   // console.log('关闭当前标签页')
+//   window.opener = null
+//   window.open('', '_self', '')
+//   window.close()
+//   window.open('', '_top')
+//   window.top.close()
+//   window.location.href = 'about:blank '
+//   window.close()
+// }
+// const middleMouseButton = () => {
+//   document.querySelector('html').addEventListener('mousedown', e => {
+//     // console.log(e.button);
+//     let mouseBtn = e.button
+//     if (mouseBtn == 1) {  //左键是0，中键是1，右键是2
+//       e.preventDefault();
+//       e.stopPropagation()
+//       closeCurrentTab()
+//     }
+//   })
+// }
+// const rightMouseButton = () => {
+//   let clickTimes = 0
+//   let clickTimeoutId
+
+//   const mousedownPoint = {
+//     x: 0,
+//     y: 0,
+//   }
+
+
+//   document.addEventListener('mousedown', e => {
+//     mousedownPoint.x = e.clientX
+//     mousedownPoint.y = e.clientY
+//   })
+
+//   document.addEventListener('contextmenu', e => {
+//     e.preventDefault()
+//     e.stopPropagation()
+//     // window.clearTimeout(clickTimeoutId)
+
+//     if (mousedownPoint.x !== e.clientX || mousedownPoint.y !== e.clientY) {
+//       // console.log('本次和上次右键位置不一样')
+//       clickTimes = 0
+//       return
+//     }
+
+//     clickTimes += 1
+//     // console.log('位置一样，当前已点击次数', clickTimes)
+
+//     if (clickTimes === 2) {
+//       closeCurrentTab()
+//       clickTimes = 0
+//     } else {
+//       clickTimeoutId = window.setTimeout(() => {
+//         // console.log('500ms 内没有点击，重置连击次数')
+//         clickTimes = 0
+//       }, 500)
+//     }
+//   })
+// }
+// switch (mode) {
+//   case 1:
+//     middleMouseButton()
+//     break
+//   case 2:
+//     rightMouseButton()
+//     break
+// } 
+
+
